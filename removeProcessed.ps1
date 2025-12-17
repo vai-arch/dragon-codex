13bfb9201ca57e9d1ac8fec2e5921e30c387b33c
vai-arch <victordiazcaballeroai@gmail.com>
2025-12-17 18:31:01 +0100

    limpieza de ficheros

--- removeProcessed.ps1
+++
@@ -1,3 +0,0 @@
-Remove-Item C:\Users\victor.diaz\Documents\_AI\dragon-codex\data\raw\books\*.json
-Remove-Item C:\Users\victor.diaz\Documents\_AI\dragon-codex\data\processed\books\*.json
-Remove-Item C:\Users\victor.diaz\Documents\_AI\dragon-codex\data\metadata\books\*.json
